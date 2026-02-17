d2b13630392f2bae07e704466a9a5809190d489a
alexandrofs <alexandrofsousa@gmail.com>
2026-02-16 21:44:35 -0300

    fix: address PR comments, remove dead code, and automate DB creation

--- backend/common/src/main/java/afsdigital/grahamselect/valuation/application/repository/IntrinsicValueRepository.java
+++
@@ -1,9 +0,0 @@
-package afsdigital.grahamselect.valuation.application.repository;
-
-import afsdigital.grahamselect.valuation.domain.entities.IntrinsicValue;
-
-public interface IntrinsicValueRepository {
-
-    void save(IntrinsicValue intrinsicValue);
-
-}
