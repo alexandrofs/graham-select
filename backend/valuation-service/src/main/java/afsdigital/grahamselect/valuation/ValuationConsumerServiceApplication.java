9d4902aa78bca5993f955cc089650b6fefccd8fb
alexandrofs <alexandrofsousa@gmail.com>
2026-02-22 18:58:21 -0300

    fix(native): add reflection binding for liquibase configuration to fix startup error on native image

--- backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
+++ backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
@@ -2,8 +2,10 @@ package afsdigital.grahamselect.valuation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 
 @SpringBootApplication
+@RegisterReflectionForBinding(liquibase.configuration.LiquibaseConfiguration.class)
 public class ValuationConsumerServiceApplication {
 
     public static void main(String[] args) {
