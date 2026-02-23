beae1c3ce112a1a711beff3fe92f2dfce9c2e2c1
alexandrofs <alexandrofsousa@gmail.com>
2026-02-23 18:15:38 -0300

    refactor(native): use official GraalVM reachability metadata for mysql and liquibase instead of manual hints

--- backend/api/src/main/java/afsdigital/grahamselect/api/ApiServiceApplication.java
+++ backend/api/src/main/java/afsdigital/grahamselect/api/ApiServiceApplication.java
@@ -3,41 +3,8 @@ package afsdigital.grahamselect.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
-import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.TypeReference;
-import org.springframework.aot.hint.MemberCategory;
-
-class ApiRuntimeHints implements RuntimeHintsRegistrar {
-    @Override
-    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-        hints.resources().registerResourceBundle("com.mysql.cj.LocalizedErrorMessages");
-        hints.resources().registerPattern("com/mysql/cj/LocalizedErrorMessages*.properties");
-
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.SingleConnectionUrl"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.FailoverConnectionUrl"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.WrongArgumentException"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.UnableToConnectException"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.CJException"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.log.StandardLogger"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-    }
-}
 
 @SpringBootApplication
-@ImportRuntimeHints(ApiRuntimeHints.class)
-@RegisterReflectionForBinding({
-        liquibase.configuration.LiquibaseConfiguration.class,
-        liquibase.logging.core.LogServiceFactory.class,
-        liquibase.configuration.ConfiguredValueModifierFactory.class,
-        liquibase.ui.LoggerUIService.class
-})
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
--- backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
+++ backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
@@ -2,42 +2,8 @@ package afsdigital.grahamselect.valuation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
-import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.aot.hint.RuntimeHints;
-import org.springframework.aot.hint.TypeReference;
-import org.springframework.aot.hint.MemberCategory;
-
-class ValuationRuntimeHints implements RuntimeHintsRegistrar {
-        @Override
-        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-                hints.resources().registerResourceBundle("com.mysql.cj.LocalizedErrorMessages");
-                hints.resources().registerPattern("com/mysql/cj/LocalizedErrorMessages*.properties");
-
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.SingleConnectionUrl"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.FailoverConnectionUrl"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.WrongArgumentException"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.UnableToConnectException"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.CJException"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-                hints.reflection().registerType(TypeReference.of("com.mysql.cj.log.StandardLogger"),
-                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        }
-}
 
 @SpringBootApplication
-@ImportRuntimeHints(ValuationRuntimeHints.class)
-@RegisterReflectionForBinding({
-                liquibase.configuration.LiquibaseConfiguration.class,
-                liquibase.logging.core.LogServiceFactory.class,
-                liquibase.configuration.ConfiguredValueModifierFactory.class,
-                liquibase.ui.LoggerUIService.class
-})
 public class ValuationConsumerServiceApplication {
 
         public static void main(String[] args) {
