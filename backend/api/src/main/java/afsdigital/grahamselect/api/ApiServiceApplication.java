65396aedf8b063dcec017fed82d0b15d48b9af17
alexandrofs <alexandrofsousa@gmail.com>
2026-02-22 22:32:58 -0300

    fix(native): refactor reflection hints to avoid runtime classcast errors with mysql exceptions

--- backend/api/src/main/java/afsdigital/grahamselect/api/ApiServiceApplication.java
+++ backend/api/src/main/java/afsdigital/grahamselect/api/ApiServiceApplication.java
@@ -6,12 +6,25 @@ import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.aot.hint.MemberCategory;
 
 class ApiRuntimeHints implements RuntimeHintsRegistrar {
     @Override
     public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
         hints.resources().registerResourceBundle("com.mysql.cj.LocalizedErrorMessages");
         hints.resources().registerPattern("com/mysql/cj/LocalizedErrorMessages*.properties");
+
+        hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.SingleConnectionUrl"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+        hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.FailoverConnectionUrl"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS);
+        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.WrongArgumentException"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.UnableToConnectException"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.CJException"),
+                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
     }
 }
 
