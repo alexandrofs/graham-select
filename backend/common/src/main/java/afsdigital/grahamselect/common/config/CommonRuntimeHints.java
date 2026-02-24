8da3b1b9d561d78e8779006870ca8bb7734c7952
alexandrofs <alexandrofsousa@gmail.com>
2026-02-23 21:43:50 -0300

    fix(native): explicitly declare Liquibase internal ServiceLocator factories in CommonRuntimeHints to fix AOT missing constructors

--- backend/common/src/main/java/afsdigital/grahamselect/common/config/CommonRuntimeHints.java
+++ backend/common/src/main/java/afsdigital/grahamselect/common/config/CommonRuntimeHints.java
@@ -19,31 +19,43 @@ import org.springframework.lang.Nullable;
  */
 public class CommonRuntimeHints implements RuntimeHintsRegistrar {
 
-    @Override
-    public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
-        // Register MySQL Driver & Internal Utilities
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.jdbc.Driver"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.log.StandardLogger"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.SingleConnectionUrl"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.CJException"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("com.mysql.cj.jdbc.exceptions.SQLError"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+        @Override
+        public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+                // Register MySQL Driver & Internal Utilities
+                hints.reflection().registerType(TypeReference.of("com.mysql.cj.jdbc.Driver"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("com.mysql.cj.log.StandardLogger"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("com.mysql.cj.conf.url.SingleConnectionUrl"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("com.mysql.cj.exceptions.CJException"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("com.mysql.cj.jdbc.exceptions.SQLError"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
 
-        // Register Liquibase Configuration
-        hints.reflection().registerType(TypeReference.of("liquibase.configuration.LiquibaseConfiguration"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("liquibase.logging.core.JavaLogService"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
-        hints.reflection().registerType(TypeReference.of("liquibase.logging.core.composite.CompositeLogService"),
-                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                // Register Liquibase Configuration
+                hints.reflection().registerType(TypeReference.of("liquibase.configuration.LiquibaseConfiguration"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("liquibase.logging.core.JavaLogService"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(
+                                TypeReference.of("liquibase.logging.core.composite.CompositeLogService"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
 
-        // Liquibase ServiceLoaders
-        hints.resources().registerPattern("META-INF/services/liquibase.*");
-        hints.resources().registerPattern("db/changelog/*");
-        hints.resources().registerPattern("db/changelog/changesets/*");
-    }
+                hints.reflection().registerType(TypeReference.of("liquibase.logging.core.LogServiceFactory"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("liquibase.logging.mdc.MdcManagerFactory"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("liquibase.change.ChangeFactory"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("liquibase.command.CommandFactory"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+                hints.reflection().registerType(TypeReference.of("liquibase.plugin.AbstractPluginFactory"),
+                                MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS);
+
+                // Liquibase ServiceLoaders
+                hints.resources().registerPattern("META-INF/services/liquibase.*");
+                hints.resources().registerPattern("db/changelog/*");
+                hints.resources().registerPattern("db/changelog/changesets/*");
+        }
 }
