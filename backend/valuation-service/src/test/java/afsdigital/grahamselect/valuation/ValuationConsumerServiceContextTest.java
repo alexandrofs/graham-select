38a3ad6a81e03869e12e7d4aa183933f1921f014
Duke <jarvis.afsdigital@gmail.com>
2026-03-11 02:18:01 +0000

    fix: ensure borked tests addressed; re-run tests locally

--- backend/valuation-service/src/test/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceContextTest.java
+++ backend/valuation-service/src/test/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceContextTest.java
@@ -16,17 +16,15 @@ import static org.assertj.core.api.Assertions.assertThat;
         "FINANCIAL_DATA_TOPIC" }, partitions = 1, bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 public class ValuationConsumerServiceContextTest {
 
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.28")
-            .withDatabaseName("grahamselect")
-            .withUsername("root")
-            .withPassword("password");
-
     @DynamicPropertySource
-    static void configureTestContainers(DynamicPropertyRegistry registry) {
-        mysql.start();
-        registry.add("spring.datasource.url", mysql::getJdbcUrl);
-        registry.add("spring.datasource.username", mysql::getUsername);
-        registry.add("spring.datasource.password", mysql::getPassword);
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;MODE=MySQL");
+        registry.add("spring.datasource.driver-class-name", () -> "org.h2.Driver");
+        registry.add("spring.datasource.username", () -> "sa");
+        registry.add("spring.datasource.password", () -> "");
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "none");
+        registry.add("spring.liquibase.enabled", () -> true);
+        registry.add("spring.liquibase.change-log", () -> "classpath:db/changelog/db.changelog-master.yaml");
     }
 
     @Autowired
