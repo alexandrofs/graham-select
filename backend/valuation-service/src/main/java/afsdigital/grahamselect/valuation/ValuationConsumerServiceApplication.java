06670d1ea10296c702cf2a870f96a24e351e2ec8
alexandrofs <alexandrofsousa@gmail.com>
2026-05-17 21:44:38 -0300

    Correçao da build

--- backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
+++ backend/valuation-service/src/main/java/afsdigital/grahamselect/valuation/ValuationConsumerServiceApplication.java
@@ -3,9 +3,13 @@ package afsdigital.grahamselect.valuation;
 import afsdigital.grahamselect.common.config.CommonRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = { "afsdigital.grahamselect.common", "afsdigital.grahamselect.valuation" })
+@EnableJpaRepositories(basePackages = { "afsdigital.grahamselect.common", "afsdigital.grahamselect.valuation" })
+@EntityScan(basePackages = { "afsdigital.grahamselect.common", "afsdigital.grahamselect.valuation" })
 @ImportRuntimeHints(CommonRuntimeHints.class)
 public class ValuationConsumerServiceApplication {
 
